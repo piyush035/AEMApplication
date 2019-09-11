f5904ee568fbba515d43add3c595e9b3fa640de7
Anand <anand.a.anand@capgemini.com>
2019-09-11 13:11:32 +1000

    added css in client lib folder for global header

--- core/src/main/java/com/aem/sample/core/models/FooterModel.java
+++ core/src/main/java/com/aem/sample/core/models/FooterModel.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Optional;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,4 +26,38 @@ public class FooterModel {
 	/** The resource resolver. */
 	@Inject
 	private ResourceResolver resourceResolver;
+	
+	@Inject
+	@Optional
+	private String footerTitle;
+	
+	@Inject
+	@Optional
+	private String logoImagePath;
+
+	@Inject
+	@Optional
+	private String logoDestinationUrl;
+	
+	
+	/**
+	 * @return the footerTitle
+	 */
+	public String getFooterTitle() {
+		return footerTitle;
+	}
+	
+	/**
+	 * @return the logoImagePath
+	 */
+	public String getLogoImagePath() {
+		return logoImagePath;
+	}
+	
+	/**
+	 * @return the logoDestinationUrl
+	 */
+	public String getLogoDestinationUrl() {
+		return logoDestinationUrl;
+	}
 }
