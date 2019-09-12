05c80e6c44db75f6e8b9a553af52f1fe9f8d3786
Anand <anand.a.anand@capgemini.com>
2019-09-12 12:33:36 +1000

    added list page component

--- core/src/main/java/com/aem/sample/core/models/PageListing.java
+++ core/src/main/java/com/aem/sample/core/models/PageListing.java
@@ -1,48 +1,107 @@
-/**
- * 
- */
 package com.aem.sample.core.models;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+
 import javax.inject.Inject;
-import org.apache.sling.api.SlingHttpServletRequest;
+import javax.jcr.Property;
+
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.Source;
+import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import java.util.Iterator;
 import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
-@Model(adaptables = SlingHttpServletRequest.class)
+@Model(adaptables = { Resource.class })
 public class PageListing {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PageListing.class);
+	/** The current resource. */
+	@SlingObject
+	private Resource currentResource;
+
+	/** The resource resolver. */
 	@Inject
-	@Source("sling-object")
 	private ResourceResolver resourceResolver;
-	private List<ListPageDetail> datafromModelList = new ArrayList<ListPageDetail>();
+
+	@Inject
+	@Optional
+	private String logoImagePath;
+
+	@Inject
+	@Optional
+	private String logoDestinationUrl;
 	
-	public List<ListPageDetail> getDatafromModelList() {
+	@Inject
+	@Optional
+	private String pagePath;
+
+	@Inject
+	@Optional
+	private Property childLinks;
+
+	public List<String> getChildLinks() {
+		final List<String> childList = new ArrayList<String>();
+//		childList.add("Anand");
+//		childList.add("Rohit Mishra");
+//		
+//		return childList;
+		
 		try {
-			Resource resource = resourceResolver.getResource("/content/AEMApplication/en");
+			Resource resource = resourceResolver.getResource(pagePath);
 			if(resource!=null)
 			{
-				Page parentPage = resource.adaptTo(Page.class);
-				if(parentPage!=null)
-				{
-					Iterator<Page> listChildPages = parentPage.listChildren();
+				PageManager pageManager = resourceResolver.adaptTo(PageManager.class);
+				Page page = pageManager.getPage(pagePath);
+				LOGGER.info("Inside getChildLinks");
+				//Page parentPage = resource.adaptTo(Page.class);
+//				if(parentPage!=null)
+//				{
+					Iterator<Page> listChildPages = page.listChildren();
 					while (listChildPages.hasNext()) {
 						Page childPage = listChildPages.next();
-						ListPageDetail detail = new ListPageDetail();
-						detail.setTitle(childPage.getTitle());
-						detail.setDescription(childPage.getDescription());
-						datafromModelList.add(detail);
+						
+						childList.add(childPage.getTitle());
+						
 					}
-				}
+					
+				//}
 			}			
 		}
 		catch(Exception e) { e.printStackTrace();}
-		return datafromModelList;
+		
+		//childList.add("Anand");
+		//childList.add("Maurya");
+		return childList;
+	}
+	
+	
+
+	/**
+	 * @return the logoImagePath
+	 */
+	public String getLogoImagePath() {
+		return logoImagePath;
+	}
+	
+	/**
+	 * @return the headerTitle
+	 */
+	public String getPagePath() {
+		return pagePath;
+	}
+
+	/**
+	 * @return the logoDestinationUrl
+	 */
+	public String getLogoDestinationUrl() {
+		return logoDestinationUrl;
 	}
 }
