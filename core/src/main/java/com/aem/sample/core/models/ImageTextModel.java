7e71573750cdabb897496d23aa25cfc1b67722d8
Piyush Gupta <piyush035@gmail.com>
2019-09-14 18:21:27 +0530

    color picker

--- core/src/main/java/com/aem/sample/core/models/ImageTextModel.java
+++ core/src/main/java/com/aem/sample/core/models/ImageTextModel.java
@@ -1,21 +1,12 @@
 package com.aem.sample.core.models;
 
-import java.util.LinkedList;
-import java.util.List;
-
 import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.aem.sample.core.dto.LinksDTO;
-import com.aem.sample.core.helper.JSONHelper;
 
 @Model(adaptables = { Resource.class })
 public class ImageTextModel {
