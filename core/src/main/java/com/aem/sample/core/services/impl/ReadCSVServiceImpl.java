90d43479b1f07f9ee171b608506fc5223f86f0ec
Anand <anand.a.anand@capgemini.com>
2019-09-26 14:20:52 +1000

    added system user

--- core/src/main/java/com/aem/sample/core/services/impl/ReadCSVServiceImpl.java
+++ core/src/main/java/com/aem/sample/core/services/impl/ReadCSVServiceImpl.java
@@ -1,16 +1,27 @@
 package com.aem.sample.core.services.impl;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.aem.sample.core.services.ReadCSVService;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.jcr.Session;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import javax.jcr.Node;
+import javax.jcr.RepositoryException;
 
-
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.LoginException;
+import org.apache.sling.api.resource.Resource;
 
 /**
  * @author Anand
@@ -22,35 +33,78 @@ public class ReadCSVServiceImpl implements ReadCSVService {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ReadCSVServiceImpl.class);
 
+	
+	@Reference    
+    private ResourceResolverFactory factory;
 	/**
 	 * Overridden method which will read the JSON data via an HTTP GET call
 	 */
+	
+	
 	@Override
 	public List<String> getCountries() {
 
-		LOGGER.info("Inside getCountries");
-		final List<String> countryList = new ArrayList<String>();
+		LOGGER.debug("Inside getCountries list method");
+		final List<String> empList = new ArrayList<String>();
 		
 		String csvFile = "/Users/anand/Desktop/countries.csv";
         String line = "";
         String cvsSplitBy = ",";
-
+        Session session = null;
+        ResourceResolver resolver = null;
+        Resource resource=null;
+        
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+        	LOGGER.debug("before resolver a");
+        	//resolver = factory.getAdministrativeResourceResolver(null);
+        	Map<String, Object> param = new HashMap<String, Object>();        
+            param.put(ResourceResolverFactory.USER, "aemapplication");
+            resolver = factory.getServiceResourceResolver(param);            
+        	LOGGER.debug("value of resolver"+resolver);
+            session = resolver.adaptTo(Session.class);
+            LOGGER.debug("value of session"+session);
+            
+            LOGGER.debug("after resolver a");
+            resource = resolver.getResource("/content/dam/AEMApplication/employeeDetails");
+
+
+            Node node = resource.adaptTo(Node.class);
+            
+            
 
             while ((line = br.readLine()) != null) {
 
                 // use comma as separator
-                String[] country = line.split(cvsSplitBy);
-
-                System.out.println("Country [code= " + country[1] + " , name=" + country[2] + "]");
-                countryList.add(country[5]);
+                String[] employee = line.split(cvsSplitBy);
+                
+                String employeeId = employee[4];
+                
+				Node newNode = node.addNode(employeeId, "nt:unstructured");
+				
+				empList.add(employee[5]);
             }
+            session.save();
 
         } catch (IOException e) {
             e.printStackTrace();
-        }
+            empList.add("IOException");
+        } catch (LoginException e) {
+			e.printStackTrace();
+			empList.add("LoginException");
+			
+			
+			
+		}
+        catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			empList.add("RepositoryException");
+		}
+        
+        empList.add("Anand");
+        empList.add("fffffjhjgjgh");
 
-		return countryList;
+		return empList;
 	}
 
 }
